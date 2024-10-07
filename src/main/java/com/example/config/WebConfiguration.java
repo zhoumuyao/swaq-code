ca99dbdb69186a5462ab6d7c6557e45cd89fa973
zhao-ygxcj <zhaofeifanx@163.com>
2024-10-07 21:46:30 +0800

    上传图片既时显示

--- src/main/java/com/example/config/WebConfiguration.java
+++ src/main/java/com/example/config/WebConfiguration.java
@@ -2,16 +2,20 @@ package com.example.config;
 
 import com.example.interceptor.AuthorizeInterceptor;
 import com.example.listener.MySessionListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${IMAGE.UPLOAD.PATH}")
+    private String uploadPath;
     @Resource
     AuthorizeInterceptor interceptor;
     @Override
@@ -19,6 +23,12 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addInterceptor(interceptor)
                 .addPathPatterns("/**");
     }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/info/**")
+                .addResourceLocations(uploadPath);
+    }
+
     @Bean
     public ServletListenerRegistrationBean<MySessionListener> mySessionListener() {
         ServletListenerRegistrationBean<MySessionListener> registrationBean
